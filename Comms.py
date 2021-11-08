39d19f25f0496228bc0db1e8c3bb80598b765558
luke780440 <91250800+luke780440@users.noreply.github.com>
2021-11-08 22:41:09 +0800

    Update Comms.py

--- Comms.py
+++ Comms.py
@@ -1,27 +1,32 @@
 
-
-
+from time import sleep
 from threading import Thread
 import serial
 
-
 class Comms:
-    def __init__(self, port, bdrate, timeout):
+    def __init__(self, port, bdrate):
         self.port = port
         self.bdrate = bdrate
-        self.timeout = timeout
+        #ser = serial.Serial(self.port, self.baud_rate)
+        
+    def run(self):
+        while True:
+            '''
+            packet = ser.read()
+            packets = packet.split(chr(127))
+            self.photoResistor = packets[0]
+            self.rpm = packets[1]
+            self.temp = packets[2]
+            lightVal = int(photoResistor)/255 * 1053 #decoding packet into light value
+            fan_speed = int(lightVal/1053 * 255) #converting to fan speed;  
+            new_packet = ser.write(fan_speed)
+            sleep(3)
+            '''
 
-
-    def run():
-        packet = ser.read()
-        lightVal = int(packet)/255 * 1053 #decoding packet into light value
-        fan_speed = int(lightVal/1053 * 255) #converting to fan speed;  
-        new_packet = ser.write(fan_speed)
-
-
-    def start_thread():
-        start_thread = Thread(target = run) #initializing the thread 
+    def start_thread(self):
+        start_thread = Thread(target = self.run) #initializing the thread 
         start_thread.start()
-        start_thread.join()
+        
 
-
+ob = Comms("/dev/tty.usbmodem14201", 9600)
+ob.start_thread()
