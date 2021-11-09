ed9775f4ea25d7dc467858f2d956c724cb2c1bac
luke780440 <91250800+luke780440@users.noreply.github.com>
2021-11-09 15:51:10 +0800

    Update Comms.py

--- Comms.py
+++ Comms.py
@@ -7,8 +7,8 @@ class Comms:
         self.port = port
         self.baud_rate = baud_rate
         ser = serial.Serial(self.port, self.baud_rate)
-        ser.open()
         ser.close()
+        ser.open()
         
     def run(self):
         while True:
